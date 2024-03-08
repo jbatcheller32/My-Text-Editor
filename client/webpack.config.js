e9aca9dbca49f621944219cf808f78b1b8f61e2b
J Mac <jbatcheller123@gmail.com>
2024-03-08 13:28:16 -0700

    setup the webpack config file to install needed plugins

---
+++ client/webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+
+      new WebpackPwaManifest({
+        name: 'Text Editor', 
+        short_name: 'Text Editor', 
+        description: 'Text editor that you can use offline',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('client/src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+           
+          },
+        ]
+      }), 
+      new InjectManifest({ 
+        swSrc: 'src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+
+      
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        
+      ],
+    },
+  };
+};
